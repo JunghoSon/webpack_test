1a270a6d197cc75d1c0a5702f3217e1dc7cc6ab1
jhson <jhson@encar.com>
2016-10-26 17:13:49 +0900

    crlf test

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ const CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
 const configBuild = require('./webpack.config.build.js');
 const configDev = require('./webpack.config.dev.js');
 const configDevIe = require('./webpack.config.dev.ie.js');
+
 const target = process.env.npm_lifecycle_event;
 
 const common = {
